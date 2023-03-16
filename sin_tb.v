28b4c6cc10a75941f69c271e9e3f5130152a0e25
syule <94171817+EropRHu4@users.noreply.github.com>
2023-03-16 17:30:32 +0300

    Update sin_tb.v

--- sin_tb.v
+++ sin_tb.v
@@ -1,55 +1,67 @@
-module sin_tb();
-
-reg clk;
-reg rst_n;
-
-reg       [13:0]   x;
-reg [15:0] expected;
-wire      [15:0]   sin;
-wire [31:0] s;
-
-
-sin sinus
-(
- .clk    (clk),
- .rst_n  (rst_n),
- .x      (x),
- .s      (s),
- .sin    (sin)
-);
-
-always begin                            
-#10;                                    
-clk = ~clk;                             
-end
-reg [15 : 0] sin_table [4095 : 0];
-
-initial begin
-
-$readmemb("D:/Verilog/SIN/memory.mem", sin_table);
- 
-end
-initial begin
-    clk = 1'b0;
-    rst_n = 0;
-    //x = (14'hffe << 2) + 1;
-    for (integer i = 0; i < 256; i = i + 1) begin
-        x = (i << 2) + 0;
-        expected = sin_table[i]; //+0
-        //expected = ((sin_table[i] + sin_table[i+1]) >> 1);  //+1
-        //expected = ((sin_table[i] + sin_table[i+1]) >> 1); //+2
-        //expected = ((sin_table[i] + sin_table[i+1]) >> 1); //+3
-        $display("X %0X : INDEX0 %0D, INDEX1 %0D", x, `INDEX0, `INDEX1);
-        if (sin != expected)
-            $display("ERROR: expected %0X calculated %0X", expected, sin);
-  /*      $display("X %0X : INDEX0 %0D, INDEX1 %0D", x, `INDEX0, `INDEX1);
-        $display("SLAG1 %0X  %0b", `SLAG1, `SLAG1);
-        $display("SLAG2 %0X  %0b", `SLAG2, `SLAG2);
-        $display("SIN %0X  %0b", s, s);
-        $display("===========================");*/
-    end
-    #100;
-    rst_n = 1;
-end
-
-endmodule
+module sin_tb();                                                                                                                            
+                                                                                                                                            
+reg clk;                                                                                                                                    
+reg rst_n;                                                                                                                                  
+                                                                                                                                            
+reg       [13:0]   x;                                                                                                                       
+reg [15:0] expected;                                                                                                                        
+wire      [15:0]   sin;                                                                                                                     
+wire [31:0] s;                                                                                                                              
+                                                                                                                                            
+                                                                                                                                            
+sin sinus                                                                                                                                   
+(                                                                                                                                           
+ .clk    (clk),                                                                                                                             
+ .rst_n  (rst_n),                                                                                                                           
+ .x      (x),                                                                                                                               
+ .s      (s),                                                                                                                               
+ .sin    (sin)                                                                                                                              
+);                                                                                                                                          
+                                                                                                                                            
+always begin                                                                                                                                
+#10;                                                                                                                                        
+clk = ~clk;                                                                                                                                 
+end                                                                                                                                         
+reg [15 : 0] sin_table [4095 : 0];                                                                                                          
+                                                                                                                                            
+initial begin                                                                                                                               
+    $readmemb("D:/Verilog/SIN/memory.mem", sin_table);                                                                                      
+end                                                                                                                                         
+                                                                                                                                            
+parameter TABLE_OFFSET = 0;                                                                                                                 
+                                                                                                                                            
+task test_all();                                                                                                                            
+integer i;                                                                                                                                  
+begin                                                                                                                                       
+  for (i = 0; i < 4096; i = i + 1) begin                                                                                                    
+        x = (i << 2) + TABLE_OFFSET;                                                                                                        
+                                                                                                                                            
+        case(TABLE_OFFSET)                                                                                                                  
+            'd0: expected = (('d4*sin_table[i] + 'd0*sin_table[i+1]) >> 'd2);  //+1                                                         
+            'd1: expected = (('d3*sin_table[i] + 'd1*sin_table[i+1]) >> 'd2);  //+1                                                         
+            'd2: expected = (('d2*sin_table[i] + 'd2*sin_table[i+1]) >> 'd2); //+2                                                          
+            'd3: expected = (('d1*sin_table[i] + 'd3*sin_table[i+1]) >> 'd2); //+3                                                          
+       endcase                                                                                                                              
+       #41;                                                                                                                                 
+        $display("X %0X : INDEX0 %0D, INDEX1 %0D  Expected %0X  Actual %0X", x, `INDEX0, `INDEX1, expected, sin);                           
+        if (sin != expected)                                                                                                                
+            $display("ERROR: expected %16b calculated %16b", expected, sin);                                                                
+//        $display("X %0X : INDEX0 %0D, INDEX1 %0D", x, `INDEX0, `INDEX1);                                                                  
+//            $display("SLAG1 %0X  %0b", `SLAG1, `SLAG1);                                                                                   
+//            $display("SLAG2 %0X  %0b", `SLAG2, `SLAG2);                                                                                   
+                                                                                                                                            
+//        $display("SIN %0X  %0b", s, s);                                                                                                   
+    end                                                                                                                                     
+    $display("END ===========================");                                                                                            
+end                                                                                                                                         
+                                                                                                                                            
+endtask                                                                                                                                     
+                                                                                                                                            
+initial begin                                                                                                                               
+    clk = 1'b0;                                                                                                                             
+    //test_all();                                                                                                                           
+    //x = 14'hff4 << 2;                                                                                                                     
+end                                                                                                                                         
+                                                                                                                                            
+endmodule                                                                                                                                   
+                                                                                                                                            
